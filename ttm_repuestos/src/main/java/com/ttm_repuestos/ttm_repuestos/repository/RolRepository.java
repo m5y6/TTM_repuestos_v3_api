621ea20164fd6842a2248eef13a066e8c0db44e9
m5y6 <ben.cerdap@duocuc.cl>
2025-10-28 16:59:43 -0300

    api clases repository service
    se inegro lo basico pero aun falta definirlas bien debido a el la relacion de las tablas

---
+++ ttm_repuestos/src/main/java/com/ttm_repuestos/ttm_repuestos/repository/RolRepository.java
@@ -0,0 +1,7 @@
+package com.ttm_repuestos.ttm_repuestos.repository;
+
+import com.ttm_repuestos.ttm_repuestos.model.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Long> {
+}
