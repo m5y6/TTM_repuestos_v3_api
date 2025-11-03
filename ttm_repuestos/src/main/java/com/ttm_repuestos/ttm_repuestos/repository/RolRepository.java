e689a7b2c15594b2ebb0e39f56086ef1daa9f22f
m5y6 <ben.cerdap@duocuc.cl>
2025-11-03 15:31:12 -0300

    Delete ttm_repuestos directory
    se elimino ya que ns por que se agrego

--- ttm_repuestos/src/main/java/com/ttm_repuestos/ttm_repuestos/repository/RolRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ttm_repuestos.ttm_repuestos.repository;
-
-import com.ttm_repuestos.ttm_repuestos.model.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolRepository extends JpaRepository<Rol, Long> {
-}
--- ttm_repuestos/src/main/java/com/ttm_repuestos/ttm_repuestos/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ttm_repuestos.ttm_repuestos.repository;
-
-
-import com.ttm_repuestos.ttm_repuestos.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByCorreo(String correo);
-}
